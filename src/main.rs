6e7f5c3fecbd771bcda85332f4f469d1df6854ea
Yami-no-karuro <carloborgna98@gmail.com>
2025-04-17 17:07:59 +0200

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ use tokenizer::{Token, Tokenizer};
 use std::collections::HashMap;
 
 fn main() {
-    let input: String = "Hello! My Name is Carlo, i'm 26 years old and i'm a software developer!".to_string();
+    let input: String = "Hello! My Name is Carlo, i'm 26 years old and i'm a software developer!"
+        .to_string();
 
     let mut tokenizer: Tokenizer = Tokenizer::new(input);
     let mut bag: HashMap<Token, usize> = HashMap::new();
