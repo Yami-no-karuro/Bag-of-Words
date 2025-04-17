fe4763d9ecd7584575858418c45ea39230a6f27b
Yami-no-karuro <carloborgna98@gmail.com>
2025-04-17 16:11:38 +0200

    Types

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@
 
 mod tokenizer;
 
-use tokenizer::{Tokenizer};
+use tokenizer::{Token, Tokenizer};
 
 fn main() {
-    let input = "Hello! My Name is Carlo, i'm 26 years old and i'm a software developer!".to_string();
-    let mut tokenizer = Tokenizer::new(input);
-    let tokens = tokenizer.tokenize();
+    let input: String = "Hello! My Name is Carlo, i'm 26 years old and i'm a software developer!".to_string();
+    let mut tokenizer: Tokenizer = Tokenizer::new(input);
+    let tokens: Vec<Token> = tokenizer.tokenize();
 
     dbg!(tokens);
 }
