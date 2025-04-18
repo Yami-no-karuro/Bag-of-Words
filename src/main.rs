d0c3315d272346e3edd509aee9393ee0823c727b
Yami-no-karuro <carloborgna98@gmail.com>
2025-04-18 09:34:52 +0200

    embed concept impl

--- src/main.rs
+++ src/main.rs
@@ -27,13 +27,32 @@ fn train(sentences: &[&str]) -> HashMap<Token, usize> {
     return bag;
 }
 
+fn embed(sentence: &str, bag: &HashMap<Token, usize>) -> Vec<f32> {
+    let input: String = sentence.to_string();
+
+    let mut tokenizer: Tokenizer = Tokenizer::new(input.to_string());
+    let input_tokens: Vec<Token> = tokenizer.tokenize();
+
+    let mut embedding: Vec<f32> = Vec::with_capacity(bag.len());
+    for bag_tokens in bag.keys() {
+        if input_tokens.contains(bag_tokens) {
+            let freq: &usize = bag.get(bag_tokens).unwrap();
+            embedding.push(1.0 / (*freq as f32));
+        } else {
+            embedding.push(0.0);
+        }
+    }
+
+    return embedding;
+}
+
 fn main() {
     let sentences: Vec<&str> = Vec::from([
         "Hello, how are you?",
         "I'm good, thank you very much!",
         "How's it going today?",
         "Today the weather is really nice.",
-        "I enjoy programming in Python.",
+        "I enjoy programming in Rust.",
         "I just finished reading an interesting book.",
         "What do you think about the new movie?",
         "Shall we go for a coffee together?",
@@ -53,4 +72,8 @@ fn main() {
 
     let bag: HashMap<Token, usize> = train(&sentences);
     println!("BoW: {:?}", bag);
+
+    let input: &str = "I enjoy programming!";
+    let embedding: Vec<f32> = embed(input, &bag);
+    println!("Embedding: {:?}", embedding);
 }
