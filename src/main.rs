8ad956e6a59c8d972d3a37ad06ba8ae12e117fa3
Yami-no-karuro <carloborgna98@gmail.com>
2025-05-16 22:01:37 +0200

    Corpus Update

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,11 @@ use tokenizer::{
 
 fn main() {
     let titles: Vec<&str> = Vec::from([
-        "Hello, how are you?",
-        "I'm good, thank you very much!",
-        "How's it going today?",
-        "Today the weather is really nice.",
-        "I enjoy programming in Rust."
+        "Il romanzo ha inizio con la festa del 111º compleanno di Bilbo e del 33° di suo nipote Frodo.",
+        "Alla fine della festa, Bilbo comunica a tutti i presenti che intende lasciare la Contea per sempre e, dopo essersi infilato l'anello, sparisce.",
+        "Lo stregone comincia a sospettare della natura dell'anello, perciò consiglia a Frodo di non adoperarlo mai e si allontana da Casa Baggins alla ricerca della verità.",
+        "Diciassette anni dopo, Gandalf scopre che l'anello in possesso di Frodo è l'Unico Anello, forgiato molti anni prima dall'Oscuro Signore Sauron.",
+        "Sauron tuttavia lo perse dopo essere stato sconfitto dal re elfico Gil-galad e dal re dei Dúnedain Elendil al culmine della battaglia dell'Ultima Alleanza tra Elfi e Uomini."
     ]);
 
     for title in titles {
